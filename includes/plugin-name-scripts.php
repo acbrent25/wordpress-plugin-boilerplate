ada0155d82ad6709d34c4a7b3314dbfb69fd62ff
acbrent25 <adamchampagne@gmail.com>
2019-04-09 04:56:48 -0400

    update naming

--- includes/plugin-name-scripts.php
+++ includes/plugin-name-scripts.php
@@ -3,8 +3,8 @@
 // Add Scripts
 
 function PREFIX_add_scripts(){
-    wp_enqueue_style('PREFIX-main-style', plugins_url() . '/social-links-widget/css/style.css');
-    wp_enqueue_script('PREFIX-main-script', plugins_url() . '/social-links-widget/js/main.js');
+    wp_enqueue_style('PREFIX-main-style', plugins_url() . '/wordpress-plugin-boilerplate/css/style.css');
+    wp_enqueue_script('PREFIX-main-script', plugins_url() . '/wordpress-plugin-boilerplate/js/main.js');
 }
 
 add_action('wp_enqueue_scripts', 'PREFIX_add_scripts');
